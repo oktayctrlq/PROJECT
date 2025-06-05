ae317d8b7e8d7717e1e14f2091427d319fb0fb4f
oktayctrlq <oktayduman2001@hotmail.com>
2025-06-05 17:38:53 +0300

    Change logging format to include timestamp

--- src/sp211/shortest_path.py
+++ src/sp211/shortest_path.py
@@ -1,7 +1,7 @@
 import heapq
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 logger = logging.getLogger(__name__)
 
 
